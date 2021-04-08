bfade82402ab19faa76937a83be134004095e390
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-04-08 14:21:47 -0500

    screen files

---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {global} from '../styles/global';
+
+const Home = () =>{
+    return (
+        <View style={global.container}>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
---
+++ screens/Task.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {global} from '../styles/global';
+
+const Task = () =>{
+    return (
+        <View style={global.container}>
+            <Text>Task</Text>
+        </View>
+    )
+}
+
+export default Task;
